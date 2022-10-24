cce79f1a0bae2151389477c508af6c1102184d20
hugo.zepeda <A01228679@exatec.tec.mx>
2022-10-24 15:13:12 -0500

    RESTful APIs with Spring Data REST

--- Creating_Your_First_SpringBoot_Microservice/explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ Creating_Your_First_SpringBoot_Microservice/explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -2,7 +2,39 @@ package com.example.ec.repo;
 
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(@Param("code")String code);
 
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
